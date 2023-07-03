97a689a40407acf7b84a29d9920c5216ea7c960c
Andrew Nasibullin <duhich02@yandex.ru>
2023-07-03 10:22:46 +0300

    4.2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
+import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { category } from './category/entities/category.entity';
+import { post } from './posts/entities/post.entity';
+import { user } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [CategoryModule,
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DBNAME,
+      entities: [category, post, user],
+      synchronize: true,
+    }),
+    UsersModule,
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
---
+++ src/posts/entities/post.entity.ts
@@ -0,0 +1,32 @@
+import {Entity, Column, PrimaryGeneratedColumn, ManyToOne} from 'typeorm';
+import {Category} from 'src/category/entities/category.entity'
+
+export enum postStatusEnum{
+    DRAW = 'Черновик',
+    PUBLISHED = 'Опубликован',
+    DELETED = 'Снят с публикации'
+}
+
+@Entity()
+export class post {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @ManyToOne(type => Category, category => category.posts, {eager: true})
+    category: Category
+
+    @Column({
+        type: 'enum',
+        enum: postStatusEnum,
+        default: postStatusEnum.DRAW
+    })
+    status: postStatusEnum
+
+    @Column({
+        type: 'datetime'
+    })
+    changed_at: Date
+}
